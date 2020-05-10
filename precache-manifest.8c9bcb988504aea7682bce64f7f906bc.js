4584bc8723693a93080d54e8aec737143055b196
7777ty <568318359@qq.com>
2020-05-10 23:02:10 +0800

    deploy

---
+++ precache-manifest.8c9bcb988504aea7682bce64f7f906bc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7098ecf25af535107510",
+    "url": "/banjin-blog-website/css/app.62c8ded6.css"
+  },
+  {
+    "revision": "618dfefb02a4392d82fa",
+    "url": "/banjin-blog-website/css/chunk-vendors.5ad50e34.css"
+  },
+  {
+    "revision": "af6963b42ba0e90b25d5",
+    "url": "/banjin-blog-website/css/view-Create-vue.cf09975e.css"
+  },
+  {
+    "revision": "008d3f9e1eebb0c5bc93",
+    "url": "/banjin-blog-website/css/view-Details-vue.7fa04f6b.css"
+  },
+  {
+    "revision": "5e1ac853edd714049546",
+    "url": "/banjin-blog-website/css/view-Edit-vue.dd044d5e.css"
+  },
+  {
+    "revision": "9cc367b08a8dc695ce74",
+    "url": "/banjin-blog-website/css/view-Edit-vue~view-MyBlogs-vue~view-UsersBlogs-vue.a5b5d932.css"
+  },
+  {
+    "revision": "26f3576b3878a9910611",
+    "url": "/banjin-blog-website/css/view-Home-vue.26a9a973.css"
+  },
+  {
+    "revision": "fe05287ce794637701cb",
+    "url": "/banjin-blog-website/css/view-Login-vue.8aa27106.css"
+  },
+  {
+    "revision": "526bb73f0bda89305f91",
+    "url": "/banjin-blog-website/css/view-MyBlogs-vue.823b5c70.css"
+  },
+  {
+    "revision": "f304d06ac79f7eb7b6fe",
+    "url": "/banjin-blog-website/css/view-SignUp-vue.6e8eb089.css"
+  },
+  {
+    "revision": "045b367ffbfc2abd10f5",
+    "url": "/banjin-blog-website/css/view-UsersBlogs-vue.1c97bc46.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/banjin-blog-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/banjin-blog-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "8ac994668c77682a5cd81abc5a80ee26",
+    "url": "/banjin-blog-website/index.html"
+  },
+  {
+    "revision": "7098ecf25af535107510",
+    "url": "/banjin-blog-website/js/app.6752357b.js"
+  },
+  {
+    "revision": "618dfefb02a4392d82fa",
+    "url": "/banjin-blog-website/js/chunk-vendors.935cacb0.js"
+  },
+  {
+    "revision": "af6963b42ba0e90b25d5",
+    "url": "/banjin-blog-website/js/view-Create-vue.31ac617e.js"
+  },
+  {
+    "revision": "d07209bd9fdc30377711",
+    "url": "/banjin-blog-website/js/view-Create-vue~view-Details-vue~view-Edit-vue~view-Home-vue~view-Login-vue~view-MyBlogs-vue~view-No~9723708e.6e40508a.js"
+  },
+  {
+    "revision": "008d3f9e1eebb0c5bc93",
+    "url": "/banjin-blog-website/js/view-Details-vue.53a7dceb.js"
+  },
+  {
+    "revision": "5e1ac853edd714049546",
+    "url": "/banjin-blog-website/js/view-Edit-vue.341807ac.js"
+  },
+  {
+    "revision": "9cc367b08a8dc695ce74",
+    "url": "/banjin-blog-website/js/view-Edit-vue~view-MyBlogs-vue~view-UsersBlogs-vue.5416abcf.js"
+  },
+  {
+    "revision": "26f3576b3878a9910611",
+    "url": "/banjin-blog-website/js/view-Home-vue.6eb2156d.js"
+  },
+  {
+    "revision": "fe05287ce794637701cb",
+    "url": "/banjin-blog-website/js/view-Login-vue.fb95aa64.js"
+  },
+  {
+    "revision": "526bb73f0bda89305f91",
+    "url": "/banjin-blog-website/js/view-MyBlogs-vue.46455ec0.js"
+  },
+  {
+    "revision": "e7506fe0c55939ff9c19",
+    "url": "/banjin-blog-website/js/view-NotFound-vue.33b83451.js"
+  },
+  {
+    "revision": "f304d06ac79f7eb7b6fe",
+    "url": "/banjin-blog-website/js/view-SignUp-vue.b38f4887.js"
+  },
+  {
+    "revision": "045b367ffbfc2abd10f5",
+    "url": "/banjin-blog-website/js/view-UsersBlogs-vue.04344bb9.js"
+  },
+  {
+    "revision": "d2388e576e72b173e187412090f2b86f",
+    "url": "/banjin-blog-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/banjin-blog-website/robots.txt"
+  }
+]);
